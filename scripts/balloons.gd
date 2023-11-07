7aa54d5c39e54e8051c0bfd4e96110525e596a86
JD Calvelli <jdcalvelli@JDs-MacBook-Pro.local>
2023-11-06 20:16:55 -0500

    adding balloon disappear

--- scripts/balloons.gd
+++ scripts/balloons.gd
@@ -3,11 +3,15 @@ extends Area2D
 var numBalloons:int = 3
 
 func _ready():
+	$AnimatedSprite2D.frame = 2
 	self.body_entered.connect(_on_balloon_entered)
 
 func _on_balloon_entered(body:Node2D):
 	numBalloons -= 1
-	# change sprite to be one with less balloons
 	print(numBalloons)
-
+	# change sprite to be one with less balloons
+	$AnimatedSprite2D.frame = numBalloons - 1
+	if numBalloons == 0:
+		visible = false
+ 
 
